2f8a804fe72bacecd868a4b6288df8307bfa824d
Oleksandr Kyshchyk <o.s.kyshchyk@student.khai.edu>
2026-05-06 01:00:44 +0300

    feat: add IDataStorage interface for persistence layer

---
+++ Carpooling.Core/Interfaces/IDataStorage.cs
@@ -0,0 +1,19 @@
+﻿using Carpooling.Core.Models;
+using System.Collections.Generic;
+
+namespace Carpooling.Core.Interfaces
+{
+    /// <summary>
+    /// Інтерфейс для керування збереженням та завантаженням даних системи
+    /// </summary>
+    public interface IDataStorage
+    {
+        // Робота з користувачами
+        void SaveUsers(IEnumerable<User> users);
+        IEnumerable<User> LoadUsers();
+
+        // Робота з поїздками
+        void SaveTrips(IEnumerable<Trip> trips);
+        IEnumerable<Trip> LoadTrips();
+    }
+}
