e808d23cbf629ea8895554c33d298476e6fb9b8d
Oleksandr Kyshchyk <o.s.kyshchyk@student.khai.edu>
2026-05-06 01:38:32 +0300

    feat: implement UserManager and TripManager for business logic layer

---
+++ Carpooling.Core/Managers/TripManager.cs
@@ -0,0 +1,52 @@
+﻿using Carpooling.Core.Interfaces;
+using Carpooling.Core.Models;
+using Carpooling.Core.Validators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Carpooling.Core.Managers
+{
+    public class TripManager
+    {
+        private readonly IDataStorage _storage;
+        private List<Trip> _trips;
+
+        public TripManager(IDataStorage storage)
+        {
+            _storage = storage;
+            _trips = _storage.LoadTrips().ToList();
+        }
+
+        // Створення поїздки водієм
+        public bool CreateTrip(Trip newTrip)
+        {
+            // Тут ми можемо додати перевірку через TripValidator у майбутньому
+            if (newTrip.DepartureTime < DateTime.Now || newTrip.Price <= 0)
+                return false;
+
+            _trips.Add(newTrip);
+            _storage.SaveTrips(_trips);
+            return true;
+        }
+
+        // Пошук поїздок за містом прибуття (використання LINQ)
+        public List<Trip> SearchTrips(string destination)
+        {
+            return _trips
+                .Where(t => t.ArrivalCity.Contains(destination, StringComparison.OrdinalIgnoreCase)
+                            && t.Status == "Активна")
+                .ToList();
+        }
+
+        // Сортування поїздок за ціною (використання IComparable)
+        public List<Trip> GetSortedTrips()
+        {
+            var sortedList = _trips.ToList();
+            sortedList.Sort(); // Викличе CompareTo в моделі Trip
+            return sortedList;
+        }
+
+        public List<Trip> GetAllTrips() => _trips;
+    }
+}
---
+++ Carpooling.Core/Managers/UserManager.cs
@@ -0,0 +1,45 @@
+﻿using Carpooling.Core.Interfaces;
+using Carpooling.Core.Models;
+using Carpooling.Core.Validators;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Carpooling.Core.Managers
+{
+    public class UserManager
+    {
+        private readonly IDataStorage _storage;
+        private List<User> _users;
+
+        public UserManager(IDataStorage storage)
+        {
+            _storage = storage;
+            // Завантажуємо існуючих користувачів при старті
+            _users = _storage.LoadUsers().ToList();
+        }
+
+        public bool Register(User newUser)
+        {
+            // Використовуємо валідатор (>= 8 символів)
+            if (!DataValidator.IsValidLogin(newUser.Login) ||
+                !DataValidator.IsValidPassword(newUser.Password))
+                return false;
+
+            // Перевіряємо унікальність логіна
+            if (_users.Any(u => u.Login == newUser.Login))
+                return false;
+
+            _users.Add(newUser);
+            _storage.SaveUsers(_users);
+            return true;
+        }
+
+        public User Login(string login, string password)
+        {
+            // Пошук користувача за парою логін/пароль
+            return _users.FirstOrDefault(u => u.Login == login && u.Password == password);
+        }
+
+        public List<User> GetAllUsers() => _users;
+    }
+}
